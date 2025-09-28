2d11cf35264f737cb6ba638b97e0971f873bb702
ayman f. ali <ayman4swd@gmail.com>
2025-09-28 20:12:39 +0300

    feat: add role-based authentication

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\AuthController;
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
+
+Route::middleware(['auth:sanctum', 'role:administrator'])->group(function () {
+    Route::get('/admin/dashboard', [AdminController::class, 'index']);
+});
